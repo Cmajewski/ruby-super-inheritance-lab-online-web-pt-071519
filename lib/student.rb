89ee170c59755548d3281dd0cdcb8ff4ba2a581e
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-09-11 03:26:59 +0000

    Automatically backed up by Learn

---
+++ lib/student.rb
@@ -0,0 +1,14 @@
+class Student
+  def hello
+    "Hey there! I'm so excited to learn stuff."
+  end
+
+  def raise_hand
+    counter=0
+    while counter<10
+      puts "Pick me!"
+      counter +=1
+    end
+  end
+  
+end
